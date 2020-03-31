0d55953ec6409ea0f0abaf57427477b8575c5e4d
Bo Zhang <bo@gradle.com>
2020-03-31 08:46:36 +0800

    Make TC webhook handler singleton

--- src/main/kotlin/org/gradle/bot/webhookhandlers/TeamCityWebHookHandler.kt
+++ src/main/kotlin/org/gradle/bot/webhookhandlers/TeamCityWebHookHandler.kt
@@ -4,7 +4,9 @@ import io.vertx.core.Handler
 import io.vertx.ext.web.RoutingContext
 import org.gradle.bot.endWithJson
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class TeamCityWebHookHandler @Inject constructor() : Handler<RoutingContext> {
     override fun handle(context: RoutingContext?) {
         context.parseTeamCityPayload()
